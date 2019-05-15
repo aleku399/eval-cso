083abfdd65f3f38648385c64503a99f414f10011
aleku399 <aleku399@gmail.com>
2019-05-15 11:09:44 +0300

    feat: use object styles indtead of strings

--- src/views/components/RangeSlider/index.tsx
+++ src/views/components/RangeSlider/index.tsx
@@ -1,59 +1,58 @@
-import styled from "@emotion/styled";
+import { css } from '@emotion/core'
 import React from "react";
+// import styled from "@emotion/styled";
+import { Input } from "semantic-ui-react";
 
-export const SliderContainer = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 0.3125rem;
-`;
-
-export const Slider = styled.input`
-  ${SliderContainer} & {
-    -webkit-appearance: none;
-    outline: none;
-    background: #175ca5;
-    height: 0.375rem;
-    width: 12.5rem;
-    border-radius: 0.3125rem;
-    &::-webkit-slider-thumb {
-      -webkit-appearance: none;
-      width: 1.125rem;
-      height: 1.125rem;
-      border-radius: 50%;
-      background: #175ca5;
+export const slider = css({
+    WebkitAppearance: "none",
+    outline: "none",
+    background: "#175ca5",
+    height: "0.375rem",
+    width: "12.5rem",
+    borderRadius: "0.3125rem",
+    '& ::webkit-slider-thumb': {
+      WebkitAppearance: "none",
+      width: "1.125rem",
+      height: "1.125rem",
+      borderRadius: "50%",
+      background: "red"
     }
-  }
-`;
+});
 
-export const Span = styled.span`
-  ${SliderContainer} & {
-    background: #175ca5;
-    color: #fff;
-    border-radius: 0.125rem;
-    padding: 0.1875rem 0.4375rem;
-    margin: 0rem 0.625rem;
-    text-align: center;
-    position: relative;
-    &::before {
-      content: "";
-      position: absolute;
-      left: -0.75rem;
-      top: 50%;
-      transform: translateY(-50%);
-      height: 0;
-      width: 0;
-      border: solid 0.375rem #175ca5;
-      z-index: -1;
-      border-top-color: white;
-      border-bottom-color: white;
-      border-left-color: white;
+export const span = css({
+    background: "red",
+    color: "red",
+    borderRadius: "0.125rem",
+    padding: "0.1875rem 0.4375rem",
+    margin: "0rem 0.625rem",
+    textAlign: "center",
+    position: "relative",
+    '& :before': {
+      content: "",
+      position: "absolute",
+      left: "-0.75rem",
+      top: "50%",
+      transform: "translateY(-50%)",
+      height: 0,
+      width: 0,
+      border: "solid 0.375rem #175ca5",
+      zIndex: -1,
+      borderTopColor: "white",
+      borderBottomColor: "white",
+      borderLeftColor: "white"
     }
-  }
-`;
+});
+
+export const sliderContainer = css({
+    display: "flex",
+    alignItems: "center",
+    padding: "0.3125rem"
+});
 
 const RangeSlider = ({ value, onChange }) => (
-  <SliderContainer>
-    <Slider
+  <div css={sliderContainer}>
+    <Input
+      css={slider}
       type="range"
       min="0"
       max="10"
@@ -62,8 +61,8 @@ const RangeSlider = ({ value, onChange }) => (
       value={value}
       onChange={onChange}
     />
-    <Span>{value}</Span>
-  </SliderContainer>
+    <span css={span}>{value}</span>
+  </div>
 );
 
 export default RangeSlider;
