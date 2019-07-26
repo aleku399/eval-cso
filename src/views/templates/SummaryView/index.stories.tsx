40fabac374c7793cf4bc8d68800c3308c1fa7879
allan <epicallan.al@gmail.com>
2019-07-26 12:30:05 +0300

    chore: use right template for summary page

---
+++ src/views/templates/SummaryView/index.stories.tsx
@@ -0,0 +1,8 @@
+import { storiesOf } from "@storybook/react";
+import React from "react";
+import SummaryView from ".";
+import withReduxProvider from "../../../storybook/provider";
+
+storiesOf("Templates/DataView", module)
+  .addDecorator(withReduxProvider)
+  .add("default", () => < SummaryView />);
